4121ac09b1ad171e8cb03ae59a4b12c471744b89
Kyle Zollo Venecek <kylezollo@gmail.com>
2021-08-23 14:32:24 -0400

    fix EDI_update

--- R/EDI_update.R
+++ R/EDI_update.R
@@ -17,7 +17,7 @@ usern <- Sys.getenv("EDI_USER")
 passw <- Sys.getenv("EDI_PASS")
 
 ## Get path to generated EML for dataset
-eml_path <- here("/Users/kylezollovenecek/Box/EDI/scheduled_publish/", "eml")
+eml_path <- here("eml")
 
 ## Read in EML to update DOI
 eml_doc <- EML::read_eml(here(eml_path, "eml.xml"))
@@ -40,7 +40,7 @@ new_pnum <- paste0("edi.",dataset_id, ".", new_version)
 eml_doc$packageId <- new_pnum
 
 # Rewrite updated EML file with EDI naming convention
-eml <- EML::write_eml(eml_doc, here("/Users/kylezollovenecek/Box/EDI/scheduled_publish/", "eml", paste0(new_pnum,".xml")))
+eml <- EML::write_eml(eml_doc, here("eml", paste0(new_pnum,".xml")))
 
 ## Publish to EDI using EDIutils
 tryCatch({
